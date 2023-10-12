2114f6cd6fc0df28d60452f9979e2761d0ffa21e
aayush321123 <132215872+aayush321123@users.noreply.github.com>
2023-10-12 08:14:15 +0000

    conversion completed

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,20 @@
+let score="33";
+let score1="33abc";
+console.log(typeof score);
+let valueInnumber=Number(score);
+console.log(valueInnumber);
+console.log(typeof valueInnumber);
+console.log(typeof score1);
+let numberScore=Number(score1);
+console.log(numberScore);//nan
+console.log(typeof numberScore);
+let start=true;
+let num=Number(start);
+console.log(typeof start);
+console.log(start);
+let score=undefined;
+Number(score);
+console.log(num);
+let score="aa";
+let num=Boolean(score);
+console.log(num);
