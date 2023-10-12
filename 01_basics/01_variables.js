54ca9c0e214b56f3ad7bc6208596b3751d5e8485
aayush321123 <132215872+aayush321123@users.noreply.github.com>
2023-10-12 06:36:06 +0000

    learn var and let

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+ const accountId=14453;
+ let accountEmail="akbaayush@gmail.com";
+ var accountPassword="12345";
+ accountCity="bihar"; 
+ let accountState;
+ console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+//  not use var because problem in block scope and functional scope
