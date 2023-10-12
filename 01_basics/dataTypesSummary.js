33ad6496a663349548283fb585e0dd5fd0b2bd55
aayush321123 <132215872+aayush321123@users.noreply.github.com>
2023-10-12 10:53:55 +0000

    datatypeSummary

---
+++ 01_basics/dataTypesSummary.js
@@ -0,0 +1,16 @@
+//primitives
+// 7 types: String,number,Boolean,null,undefined,symbol,BigInt
+
+//reference(non-primitive)
+//Array,Object,Functions
+const heroes=["shaktiman","nagraaj","ironman"];
+console.log(heroes);
+let myObj={
+    name: "Aayush",
+    age:20
+}
+console.log(myObj);
+const myFunction = function(){
+    console.log("Hello World");
+}
+console.log(myFunction);
