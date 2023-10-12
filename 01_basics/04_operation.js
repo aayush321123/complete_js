1df72d1298784c78212938e10d91fa87c07eeae3
aayush321123 <132215872+aayush321123@users.noreply.github.com>
2023-10-12 08:34:11 +0000

    completed operation in javaScript

---
+++ 01_basics/04_operation.js
@@ -0,0 +1,15 @@
+console.log(2+"2");//number convert into string
+console.log('2'+2+2+2+2);//after string number convert into string
+console.log(+true);
+console.log(+"");
+let gameCounter=1;
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+let a=2;
+let b=a++;
+console.log(`a=${a} and b=${b}`);
+
+let c=2;
+let d=++c;
+console.log(`c=${c} and d=${d}`);
