6800cab9fc8730ee6bb4337faed5bf4907d80bd3
aayush321123 <132215872+aayush321123@users.noreply.github.com>
2023-10-12 06:51:40 +0000

    dataTypes completed

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict"// only for new version of javaScript
+//alert() not for node js
+//number 2 to power(53)
+//big int
+//string ""
+//null  standalone value(khali)
+//undefined value not define
+//symbol  used for uniqness
+
+//object
+console.log(typeof null);// object
+console.log(typeof undefined);// undefined
