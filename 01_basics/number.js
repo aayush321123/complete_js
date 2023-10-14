4a747829521113ecad08ed3f748623cad9b715a2
aayush321123 <132215872+aayush321123@users.noreply.github.com>
2023-10-14 09:03:57 +0000

    math and number

---
+++ 01_basics/number.js
@@ -0,0 +1,28 @@
+// const num=400;
+// console.log(num);
+// const balance=new Number(200);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+// let otherNumber=200.9292;
+// otherNumber.toPrecision(4);
+// console.log(otherNumber.toPrecision(5));
+// console.log(typeof otherNumber);
+
+// const salary=10000000;
+// console.log(salary.toLocaleString('en-IN'));
+
+
+// ++++++++Maths+++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-23));
+// console.log(Math.round(4.9));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.999999999999999));
+console.log(Math.floor((Math.random()*10))+1);
+let min=10;
+let max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min);
