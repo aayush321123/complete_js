191a45daa47018f49f555f26cc3eef9a2fd565f7
aayush321123 <132215872+aayush321123@users.noreply.github.com>
2023-10-14 15:30:16 +0000

    array completed

---
+++ 02_basics/01_array.js
@@ -0,0 +1,15 @@
+// const marvel_heroes=["iron man","thor","spiderman"];
+// const dc_heroes=["catWoman","batman","superman"];
+// const all=marvel_heroes.concat(dc_heroes);
+// console.log(all);
+// const all_heroes=[...marvel_heroes,...dc_heroes];
+// const another_array=[1,2,[4,5,6,[8,89]]];
+// const real=another_array.flat(Infinity)
+// console.log(real);
+console.log(Array.isArray("Aayush"));
+console.log(Array.from("Aayush"));
+//intresting case for interview
+console.log(Array.from({name:"AAYUSH"}));
+let a=2;
+let b=3;
+console.log(Array.of(a,b));
